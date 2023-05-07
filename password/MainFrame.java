387a402b655389463d2d3a08735b653c80b156d4
steven-ngle <117310378+steven-ngle@users.noreply.github.com>
2023-05-07 18:09:24 +0200

    commit frame and panel

--- password/MainFrame.java
+++ password/MainFrame.java
@@ -1,9 +1,26 @@
 package password;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainFrame {
 
     public static void main(String[] args) {
 
         System.out.println("Moin moin");
+
+
+        // kann gerne noch geändert werden
+
+        JFrame frame = new JFrame("Passwort Generator");
+        frame.setSize(600, 400);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.blue.darker().darker().darker().darker().darker());
+
+        frame.add(panel);
+        frame.setVisible(true);
     }
+
+
 }
